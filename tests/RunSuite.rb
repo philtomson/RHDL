3e2cd4d6853d068c7f3321c71712a015a0321f61
KISHIMOTO, Makoto <ksmakoto@dd.iij4u.or.jp>
2013-02-01 13:56:54 +0900

    add path prefix to require local lib

--- tests/RunSuite.rb
+++ tests/RunSuite.rb
@@ -1,7 +1,7 @@
-require "test_syntax.rb"
+require "./test_syntax.rb"
 Dir["test*.rb"].each { |file| 
   if (file !~ /test_syntax.rb/ )
-    puts "...Running #{file}..."; require file 
+    puts "...Running #{file}..."; require "./#{file}"
   end
 }
 
